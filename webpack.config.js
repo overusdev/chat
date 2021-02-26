483d527f41dc54f6d0fb30b52564c6ef25f781d3
Сергей Вилков <vilkov@reg.ru>
2021-02-26 21:27:46 +0300

    Init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable */ 
+
+const path = require('path');
+
+module.exports = {
+    watch: true,
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+};
